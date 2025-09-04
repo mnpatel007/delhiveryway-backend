4dd74a8bedf3ea7a5c7f61a8558f3e81d1364cd1
Meet <meetnp007@gmail.com>
2025-09-04 14:11:06 -0400

    Final touches

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const orderController = require('../controllers/ordercontroller');
+const orderController = require('../controllers/OrderController');
 const authMiddleware = require('../middleware/authMiddleware');
 const shopperAuthMiddleware = require('../middleware/shopperAuthMiddleware');
 const upload = require('../middleware/uploadMiddleware');
