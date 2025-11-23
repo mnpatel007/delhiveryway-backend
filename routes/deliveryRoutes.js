d92744f4b0609c90abdd06978ef1edf6c0d7795d
Meet <meetnp007@gmail.com>
2025-11-23 05:19:57 -0500

    Delivery Discounts

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const deliveryController = require('../controllers/deliveryController');
-const { protect, adminProtect } = require('../middleware/authMiddleware');
+const { adminProtect } = require('../middleware/authMiddleware');
 
 // Public routes (or protected by user auth if needed, but usually public for cart calculation)
 router.post('/calculate-fee', deliveryController.calculateFee);
 router.post('/calculate-fees-bulk', deliveryController.calculateFeesBulk);
 
 // Admin routes for discounts
-router.get('/discounts', protect, adminProtect, deliveryController.getAllDiscounts);
-router.post('/discounts', protect, adminProtect, deliveryController.createDiscount);
-router.put('/discounts/:id', protect, adminProtect, deliveryController.updateDiscount);
-router.delete('/discounts/:id', protect, adminProtect, deliveryController.deleteDiscount);
-router.patch('/discounts/:id/toggle', protect, adminProtect, deliveryController.toggleDiscountStatus);
+router.get('/discounts', adminProtect, deliveryController.getAllDiscounts);
+router.post('/discounts', adminProtect, deliveryController.createDiscount);
+router.put('/discounts/:id', adminProtect, deliveryController.updateDiscount);
+router.delete('/discounts/:id', adminProtect, deliveryController.deleteDiscount);
+router.patch('/discounts/:id/toggle', adminProtect, deliveryController.toggleDiscountStatus);
 
 module.exports = router;
